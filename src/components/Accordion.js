d66354b8857a0265d2d1ae8979219ef24ad7d243
ohenekwabena <adjeiasarejesse@gmail.com>
2024-02-22 05:59:50 +0000

    Make accordion arrow responsive with animation

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import { DATA } from "../data/data";
 import { ChevronDown } from "react-feather";
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import UnstyledButton from "./UnstyledButton";
 
 function Accordion() {
   const [selected, setSelected] = useState();
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
   const [multiple, setMultiple] = useState([]);
-  // const [animate, setAnimate] = useState(false);
+  const [animateMap, setAnimateMap] = useState({});
 
   function handleSingleSelect(currentId) {
     setSelected(currentId === selected ? null : currentId);
+    setAnimateMap((prevState) => ({
+      ...prevState,
+      [currentId]: !prevState[currentId],
+    }));
+
+    if (selected && animateMap[selected]) {
+      setAnimateMap((prevState) => ({
+        ...prevState,
+        [selected]: false,
+      }));
+    }
   }
 
   function handleMultipleSelect(currentId) {
@@ -22,24 +33,35 @@ function Accordion() {
     else newMultiple.splice(indexOfId, 1);
 
     setMultiple(newMultiple);
+    setAnimateMap((prevState) => ({
+      ...prevState,
+      [currentId]: !prevState[currentId],
+    }));
+  }
+
+  function toggleMultiSelect() {
+    setSelected(false);
+    setAnimateMap({});
+    setMultiple([]);
+    setEnableMultiSelection(!enableMultiSelection);
   }
 
   return (
     <Wrapper>
-      <UnstyledButton onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
-        Enable Multi-selection
-      </UnstyledButton>
+      <ToggleButton onClick={toggleMultiSelect}>Enable Multi-selection</ToggleButton>
+      <h6>{enableMultiSelection ? "Multi" : "Single"}</h6>
       <div>
         {DATA?.map(({ id, question, answer }) => (
-          <InnerWrapper key={id}>
+          <div key={id}>
             <Header onClick={enableMultiSelection ? () => handleMultipleSelect(id) : () => handleSingleSelect(id)}>
               <h3>{question}</h3>
-              <Arrow />
+              <Arrow animate={animateMap[id] ? 1 : 0} />{" "}
+              {/* To avoid console error for passing custom boolean property to styled component*/}
             </Header>
             {enableMultiSelection
               ? multiple.indexOf(id) !== -1 && <div>{answer}</div>
               : selected === id && <div>{answer}</div>}
-          </InnerWrapper>
+          </div>
         ))}
       </div>
     </Wrapper>
@@ -48,7 +70,18 @@ function Accordion() {
 
 const Wrapper = styled.div`
   width: 400px;
+
+  & h6 {
+    margin: 6px 0 0 0;
+  }
 `;
+
+const ToggleButton = styled(UnstyledButton)`
+  border-radius: 6px;
+  background-color: hsl(150deg 30% 60% / 0.6);
+  padding: 6px 12px;
+`;
+
 const Header = styled(UnstyledButton)`
   margin: 0;
   border: 0;
@@ -60,10 +93,35 @@ const Header = styled(UnstyledButton)`
   align-items: center;
 `;
 
-const Arrow = styled(ChevronDown)`
-  margin-left: auto;
+const arrowRotateOut = keyframes`
+    0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-180deg);
+  }
 `;
 
-const InnerWrapper = styled.div``;
+const arrowRotateIn = keyframes`
+    100% {
+    transform: rotate(180deg);
+  }
+  0% {
+    transform: rotate(0deg);
+  }
+`;
+
+const Arrow = styled(ChevronDown)`
+  margin-left: auto;
+
+  animation: ${({ animate }) =>
+    animate
+      ? css`
+          ${arrowRotateOut} 0.2s ease-in forwards
+        `
+      : css`
+          ${arrowRotateIn} 0.2s ease-in
+        `};
+`;
 
 export default Accordion;
